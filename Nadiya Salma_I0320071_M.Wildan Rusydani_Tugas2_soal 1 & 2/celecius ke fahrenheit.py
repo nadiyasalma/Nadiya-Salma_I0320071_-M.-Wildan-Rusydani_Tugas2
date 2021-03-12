efffa1d0c9ff073234f9a6e6a7a99fd0937a4517
nadiya salma <80443317+nadiyasalma@users.noreply.github.com>
2021-03-12 23:29:14 +0700

    Add files via upload

---
+++ Nadiya Salma_I0320071_M.Wildan Rusydani_Tugas2_soal 1 & 2/celecius ke fahrenheit.py
@@ -0,0 +1,14 @@
+#suhu celcius ke farenheit
+print(" konversi suhu celcius ke farenheit")
+
+#input suhu ke celcius
+c = float(input("masukkan suhu (celcius): "))
+
+#konversi suhu
+f = (9/5 * c) + 32
+
+#hasil konversi suhu
+print("celcius: ",c)
+print("farenheit: ",f)
+
+
