09ab57beedcac406058398b818671cd52318647e
nadiya salma <80443317+nadiyasalma@users.noreply.github.com>
2021-03-12 23:27:18 +0700

    Add files via upload

---
+++ Tugas2_exercise 2.2.py
@@ -0,0 +1,32 @@
+#proses memasukkan data ke dalam variabel
+nama = "John Doe"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 20
+print(umur)
+type(umur)
+umur = "dua puluh satu"
+print(umur)
+type(umur)
+
+namaDepan = "budi"
+namaBelakang = "susanto"
+nama = namaDepan + " " + namaBelakang
+umur = 22
+hobi = "berenang"
+print("biodata\n", nama, "\n" ,umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "halo"
+ini_juga_variabel = "hai"
+_inivariabeljuga = "hi"
+inivariabel222 = "bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
+
+
