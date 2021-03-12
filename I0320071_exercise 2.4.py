1d6c3d3011597d39519add8041534d8425886055
nadiya salma <80443317+nadiyasalma@users.noreply.github.com>
2021-03-13 00:30:37 +0700

    Add files via upload

---
+++ I0320071_exercise 2.4.py
@@ -0,0 +1,12 @@
+#menampilkan informasi program
+print("konversi suhu (farenheit ke celcius)")
+
+#input suhu dalam farenheit
+F = float(input("masukkan suhu(farenheit): "))
+
+#melakukan konversi suhu ke celcius
+C = 5 * (F-32) / 9
+
+#menampilkan hasil konversi ke layar
+print("farenheit: ", F)
+print("celcius: ", C)
