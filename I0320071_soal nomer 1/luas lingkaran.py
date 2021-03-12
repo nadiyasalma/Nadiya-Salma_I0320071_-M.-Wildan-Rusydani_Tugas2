49a06d1c82ca11d92f1df2aad4b19f9e3688907c
nadiya salma <80443317+nadiyasalma@users.noreply.github.com>
2021-03-12 23:34:42 +0700

    Add files via upload

---
+++ I0320071_soal nomer 1/luas lingkaran.py
@@ -0,0 +1,6 @@
+print("menghitung luas lingkaran")
+phi = float(input("masukkan nilai phi: "))
+r = int(input("masukkan niali r: "))
+luas = (phi*r**2)
+print("luas lingkaran: ",luas," cm")
+
---
+++ I0320071_soal nomer 1/reamur ke kelvin.py
@@ -0,0 +1,12 @@
+#suhu reamur ke kelvin
+print("konversi suhu reamur ke kelvin")
+
+#input suhu ke reamur
+r = float(input("masukkan suhu(reamur): "))
+
+#konversi suhu
+k = (5/4 * r) + 273
+
+#hasil suhu konversi
+print("reamur: ", r)
+print("kelvin:" , k)
