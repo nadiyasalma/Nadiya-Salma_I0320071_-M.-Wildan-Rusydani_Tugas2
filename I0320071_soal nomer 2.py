39b033f5c03d2605b2550c64479c36dae7d25f9e
nadiya salma <80443317+nadiyasalma@users.noreply.github.com>
2021-03-13 00:32:18 +0700

    Add files via upload

---
+++ I0320071_soal nomer 2.py
@@ -0,0 +1,47 @@
+#nama
+nama = "nadiya salma rosyida"
+panggilan = "nadiya"
+hobi = "dengerin musik, travelling"
+print("hai nama saya", nama, "aku biasa dipanggil", panggilan, "hobi saya adalah", hobi)
+
+#umur
+tahun = 2 ** 2 * 4 + 7 - 4
+bulan = (2+3+3+5) * 0
+hari = 121 ** (1/2)
+print("umur saya adalah", tahun, "tahun", bulan, "bulan", hari,"hari")
+
+#alamat
+kabupaten = "boyolali"
+print("saya tinggal di", kabupaten)
+
+#pendidikan
+universitas = "universitas sebelas maret"
+fakultas = "teknik"
+jurusan = "teknik industri"
+semester = 2 * 2 + 5 - 7
+print("saya berkuliah di", universitas, "fakultas", fakultas, "tepatnya di jurusan", jurusan, "dan aku sekarang sudah semester", semester)
+
+#tinggi
+tinggi_centimeter = int(input("masukkan tinggi saya: "))
+tinggi_meter = tinggi_centimeter / 100
+tinggi_feet = tinggi_meter * 3.28
+print("tinggi saya adalah", tinggi_centimeter,"centimeter", "atau dalam meter adalah", tinggi_meter, "meter", "dan dalam feet adalah", tinggi_feet,"feet")
+
+#berat badan
+berat_kilogram = float(input("masukkan berat badan: "))
+berat_gram = berat_kilogram / 1000
+print("dan berat badan saya adalah", berat_kilogram,"kilogram", "atau", berat_gram, "gram" )
+
+#ukuran sepatu
+ukuran_eu = int(input("masukkan ukuran sepatu eu: "))
+
+
+
+
+
+
+
+
+
+
+
